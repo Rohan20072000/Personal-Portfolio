9ad70d500a37a9e5f6c28676051f85799dabfe83
Rohan20072000 <rg212873@gmail.com>
2025-08-26 14:11:49 +0530

    deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -17,6 +17,10 @@ import dj_database_url
 
 
 
+
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -84,10 +88,9 @@ CORS_ALLOW_ALL_ORIGINS = True
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL")
+    )
 }
 
 
