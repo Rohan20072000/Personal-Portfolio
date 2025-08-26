c8b581a732cfb693de72e85403ca0917eb893b70
Rohan20072000 <rg212873@gmail.com>
2025-08-26 14:16:12 +0530

    deploy

--- backend/backend/create_superuser.py
+++ backend/backend/create_superuser.py
@@ -2,14 +2,19 @@ from django.contrib.auth import get_user_model
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
 import os
+from django.db.utils import IntegrityError
 
 @receiver(post_migrate)
+
+
 def create_default_superuser(sender, **kwargs):
     User = get_user_model()
-    if not User.objects.filter(username="admin").exists():
+    username = "sohan"
+
+    if not User.objects.filter(username=username).exists():
         User.objects.create_superuser(
-            username="sohan",
+            username=username,
             email="sohan@gmail.com",
             password="sohan@123"
         )
-        print("✅ Superuser created: sohan / sohan@123")
+
